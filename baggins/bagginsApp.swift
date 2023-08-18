3e6ca0f9f4d036af369575edd07e4773b931982d
cha0tica <isska@inbox.ru>
2023-08-18 23:28:18 +0300

    Initial Commit

---
+++ baggins/bagginsApp.swift
@@ -0,0 +1,17 @@
+//
+//  bagginsApp.swift
+//  baggins
+//
+//  Created by Agata on 18.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct bagginsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
